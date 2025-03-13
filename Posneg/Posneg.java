1c3382c8b8c517c200085473f38b327325a7f860
cherin18 <cherin0405@gmail.com>
2025-03-13 12:06:50 +0530

    posneg

---
+++ Posneg/Posneg.java
@@ -0,0 +1,13 @@
+public class Posneg {
+    public static void main(String[] args) {
+        int number = -5; // You can change this value to test other numbers
+
+        if (number > 0) {
+            System.out.println(number + " is Positive.");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative.");
+        } else {
+            System.out.println(number + " is Zero.");
+        }
+    }
+}
