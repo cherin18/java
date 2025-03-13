24d9b27b8f982a772e830f316dbfb22eb355cd6f
cherin18 <cherin0405@gmail.com>
2025-03-13 11:46:39 +0530

    if else

---
+++ if else/Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+}
