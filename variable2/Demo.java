4e2c2d5169154fcde46fc475010eb6d9c8763e7b
cherin18 <cherin0405@gmail.com>
2025-03-13 10:03:29 +0530

    boolen

---
+++ variable2/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String args[]) {
+        boolean isValid = false;
+        if (isValid) {
+            System.out.println("The value is TRUE!");
+        } else {
+            System.out.println("The value is FALSE!");
+        }
+    }
+}
