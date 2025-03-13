f9cf5354407a00e54b60ec0be51a2a74444f033d
cherin18 <cherin0405@gmail.com>
2025-03-13 11:00:23 +0530

    if if

---
+++ if if/If.java
@@ -0,0 +1,45 @@
+public class If {
+    public static void main(String[] args) {
+        if (10 < 2) {
+            System.out.println("True");
+        }
+        if (!(10 < 2)) {
+            System.out.println("False");
+        }
+
+        if (10 > 2) {
+            System.out.println("True");
+        }
+        if (!(10 > 2)) {
+            System.out.println("False");
+        }
+
+        if (10 <= 2) {
+            System.out.println("True");
+        }
+        if (!(10 <= 2)) {
+            System.out.println("False");
+        }
+
+        if (10 >= 2) {
+            System.out.println("True");
+        }
+        if (!(10 >= 2)) {
+            System.out.println("False");
+        }
+
+        if (10 != 2) {
+            System.out.println("True");
+        }
+        if (!(10 != 2)) {
+            System.out.println("False");
+        }
+
+        if (10 == 2) {
+            System.out.println("True");
+        }
+        if (!(10 == 2)) {
+            System.out.println("False");
+        }
+    }
+}
