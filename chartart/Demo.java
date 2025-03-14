9c86824fb70c13026bd9bb57059960588dcec4f5
cherin18 <cherin0405@gmail.com>
2025-03-14 11:54:36 +0530

    chartat

---
+++ chartart/Demo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Demo {
+    public static void main(String args[]) {
+        String name = "hello world";
+        System.out.println (name.charAt (0));
+    }
+}
+     
