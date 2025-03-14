30bd54462a21636089a89eb5f2d1e5098d8ca678
cherin18 <cherin0405@gmail.com>
2025-03-14 11:59:14 +0530

    split

---
+++ split/Demo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Demo {
+    public static void main(String args[]) {
+        String name = "hello world"; 
+        String[] names = name.split(" "); 
+        System.out.println(names[0]); 
+    }
+}
